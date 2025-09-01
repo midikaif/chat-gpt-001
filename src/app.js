0401df4890cc240081fbdb4f076c64da9152ce82
Md Kaif <mdkaif0153@gmail.com>
2025-09-01 21:30:00 +0530

    Add chat functionality with chat model, controller, and routes; implement authentication middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
+
+const authRoutes = require('./routes/auth.routes');
+const chatRoutes = require('./routes/chat.routes');
+
 
 const app = express()
 
 app.use(express.json());
+app.use(cookieParser());
 
 
+app.use('/api/auth', authRoutes);
+app.use('/api/chat', chatRoutes);
 
 
 
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/user.model');
+
+
+async function authUser(req,res,next){
+    const {token} = req.cookies;
+
+    if(!token){
+        return res.status(401).json({message: 'Unauthorized!'})
+    }
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        
+        const user = await userModel.findById(decoded.id);
+        
+        req.user = user;
+        
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Unauthorized!'
+        })
+    }
+
+}
+
+
+module.exports = {
+    authUser
+}
